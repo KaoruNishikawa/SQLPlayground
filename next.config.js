79614869dd1d92e2e88208a912abec780c338511
Kaoru Nishikawa <kq.nishikawa@gmail.com>
2024-09-29 16:51:30 +0900

    Fix the version of WASM file to be loaded

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false }
+        return config
+    },
+    webpack5: true
 }
 
 module.exports = nextConfig
