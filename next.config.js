45f1e439550b6676b057a8eba8247990e5ac7715
Kaoru Nishikawa <68896036+KaoruNishikawa@users.noreply.github.com>
2026-03-19 23:49:53 +0900

    Update dependency versions (#28)
    * Use pnpm and upgrade package versions

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    webpack: (config) => {
-        config.resolve.fallback = { fs: false }
-        return config
-    },
-    webpack5: true
+    output: 'export'
 }
 
 module.exports = nextConfig
